081d663089ae7e33721c3816ac12c8ce000fdb46
Benderouach Hamza <hbenderouach@L530561.SQLI>
2019-12-25 20:08:58 +0100

    first commit

---
+++ src/main/java/com/humanup/adminmatrix/AdminMatrixApplication.java
@@ -0,0 +1,13 @@
+package com.humanup.adminmatrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminMatrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminMatrixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/humanup/adminmatrix/AdminMatrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humanup.adminmatrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminMatrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
